66ba111c4a61ac4ca066441ac35d3478d6c1e777
yourname <aksk5475@gmail.com>
2026-02-23 11:08:50 +0545

    build project

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,14 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
                                                       
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,                                     
-// )                           
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './',     // 🔥 REQUIRED
-  plugins: [react()],
-})                         
-
-
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,                                     
+)                           
+ 
 
 
 
