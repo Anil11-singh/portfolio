f206534f8e98a2538c2ce09c82a92093096ae5c1
yourname <aksk5475@gmail.com>
2026-03-24 12:54:30 +0545

    changes something

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   
+  plugins: [react()],
    base: '/portfolio/',
-  // plugins: [react()],
-  plugins: [react(), tailwindcss(),],
+  // plugins: [react(), tailwindcss(),],
   //   server: {
   //   host: true
   // }
