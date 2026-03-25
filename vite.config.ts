2d178eec192fe41524a60288f5c9957daa3618d6
yourname <aksk5475@gmail.com>
2026-03-25 15:02:10 +0545

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   
+     base: '/portfolio/',
   plugins: [react()],
-   base: '/portfolio/',
+   
   // plugins: [react(), tailwindcss(),],
   //   server: {
   //   host: true
