6a1edb6b9bc75dd028c24236db7f5c099c99ad60
yourname <aksk5475@gmail.com>
2026-08-12 12:25:13 +0545

    update

--- src/App.tsx
+++ src/App.tsx
@@ -139,8 +139,10 @@ export default function App() {
           </div>
 
           <div className="bg-slate-900 rounded-2xl border p-9">
-            <img src={logo} alt="Anil Kumar Mahato"
-             className="w-64 h-64 object-cover rounded-2xl mx-auto"
+            <img
+                   src={logo}
+               alt="Anil Kumar Mahato"
+                 className="w-64 h-64 object-cover rounded-2xl mx-auto"
             />
 
             <div className="bg-slate-900 shadow-md rounded-2xl p-4 hover:shadow-xl transition mt-2">
